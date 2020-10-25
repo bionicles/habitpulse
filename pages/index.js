0dddb94dfb3d84f68be8ddf2b0f99f29b9dcbfbf
Bion Howard <bion@bitpharma.com>
2020-10-25 19:25:13 -0400

    change text

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,18 @@
-import TodoForm from '../components/todo-form'
+import TodoForm from "../components/todo-form";
 
 function Index({ user }) {
   if (user) {
     return (
       <div className="w-4/5 md:w-1/2 mx-auto">
         <h3 className="font-bold text-4xl">
-          Welcome, <span className="bg-yellow-400">{user.username}</span>!
+          HabitPulse for <span className="bg-yellow-400">{user.username}</span>
         </h3>
         <TodoForm />
       </div>
-    )
+    );
   } else {
-    return null
+    return null;
   }
 }
 
-export default Index
+export default Index;
