58dcaaa102991af241c88b080511fe9f70a9d908
Bion Howard <bion@bitpharma.com>
2020-10-30 23:53:38 -0400

    add milliseconds

--- tricks/get-now.js
+++ tricks/get-now.js
@@ -1,5 +1,5 @@
 import dayjs from "dayjs";
 
-const timestampFormat = "dddd D MMMM YYYY, h:mm:ss A [UTC]Z";
+const timestampFormat = "dddd D MMMM YYYY, h:mm:ss:SSS A [UTC]Z";
 
 export const getNow = () => dayjs().format(timestampFormat);
